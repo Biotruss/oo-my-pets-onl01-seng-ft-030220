00082d666483f199d35a46d884c6d24fb8e88e5b
Craig Christopher Messi <craig.messi@gmail.com>
2020-03-19 11:55:38 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -51,7 +51,10 @@ class Owner
   def sell_pets
     pets = self.cats + self.dogs
     
-    pets.each {|pet| pet.mood = "nervous" && pet.owner = nil}
+    pets.each do |pet| 
+      pet.mood = "nervous"
+      pet.owner = nil
+    end
     
   end
     
